dc5fb6b104c4ea3d4f305a681a4929817c19e015
JachuPL <bonkowskijan@gmail.com>
2019-04-24 18:24:57 +0200

    First 3 files

---
+++ App2.java
@@ -0,0 +1,12 @@
+package pl.bonkowski;
+
+/**
+ * Hello world!
+ *
+ */
+public class App2
+{
+	public static void SayMyName() {
+		System.out.println("Jan Bońkowski");
+	}
+}
---
+++ App3.java
@@ -0,0 +1,12 @@
+package pl.bonkowski;
+
+/**
+ * Hello world!
+ *
+ */
+public class App3
+{
+	public static void SayMyAlmaMater() {
+		System.out.println("Maria Curie-Skłodowska University in Lublin");
+	}
+}
